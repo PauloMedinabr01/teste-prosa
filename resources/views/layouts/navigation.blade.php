08736f3602e4f085bad9543cd9d0c4a4432a31e8
PauloMedinabr01 <paulomedinabr01@gmail.com>
2025-02-20 20:27:35 -0300

    Ajustes na barra de navegação

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,14 +16,14 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
-            </div>
 
-            {{-- Slot de Título --}}
-            <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                <x-nav-link :href="route('users.index')"
-                            :active="request()->routeIs('users.index') || request()->routeIs('users.create') || request()->routeIs('users.edit') || request()->routeIs('users.show')">
-                    {{ __('Usuários') }}
-                </x-nav-link>
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('users.index')"
+                                :active="request()->routeIs('users.index') || request()->routeIs('users.create') || request()->routeIs('users.edit') || request()->routeIs('users.show')">
+                        {{ __('Usuários') }}
+                    </x-nav-link>
+                </div>
+
             </div>
 
             <!-- Settings Dropdown -->
