c33bfdf54963d7e91674157a915fe1775ba59361
PauloMedinabr01 <paulomedinabr01@gmail.com>
2025-02-20 22:59:21 -0300

    Ajustes nos componentes

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -10,17 +10,26 @@
                     </a>
                 </div>
 
-                <!-- Navigation Links -->
+                {{-- Dashboard --}}
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
 
+                {{-- Users --}}
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('users.index')"
                                 :active="request()->routeIs('users.index') || request()->routeIs('users.create') || request()->routeIs('users.edit')">
                         {{ __('Usuários') }}
+                    </x-nav-link>
+                </div>
+
+                {{-- Cliente --}}
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('clientes.index')"
+                                :active="request()->routeIs('clientes.index') || request()->routeIs('clientes.create') || request()->routeIs('clientes.edit')">
+                        {{ __('Clientes') }}
                     </x-nav-link>
                 </div>
 
