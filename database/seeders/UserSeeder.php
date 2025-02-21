c5c7de003e33f38729047cef904348bb39da6931
PauloMedinabr01 <paulomedinabr01@gmail.com>
2025-02-20 22:57:09 -0300

    Ajustes no UserSeeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory(20)->create();
+
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@email.com',
+        ]);
+    }
+}
